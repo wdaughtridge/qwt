b99325c831c380b842cba5dd050927d67b8f3e12
Will Daughtridge <wdaughtridge@gmail.com>
2025-04-24 21:45:51 -0400

    fix mut self

--- src/quic.rs
+++ src/quic.rs
@@ -67,7 +67,10 @@ impl QwtBidirectional {
         self.sess().await?;
 
         while let Ok(Some(data)) = self.bidi.receive().await {
-            debug!("Got WebTransport {} bytes of bidirectional data", data.len());
+            debug!(
+                "Got WebTransport {} bytes of bidirectional data",
+                data.len()
+            );
 
             let _data = self.buf.iter().chain(data.iter());
 
@@ -140,31 +143,19 @@ impl QwtClient {
         // Their SETTINGS frame will be sent to this stream
         if let Some(rx) = self.conn.accept_receive_stream().await? {
             debug!("Accepted receive stream");
-            tokio::spawn(async move {
-                QwtClient::handle_receive_control_stream(rx, tx)
-                    .await
-                    .expect("client control")
-            });
+            self.handle_receive_control_stream(rx, tx).await?
         }
 
         // Our SETTINGS frame will be sent from this stream
         if let Ok(tx) = self.conn.open_send_stream().await {
             debug!("Opened send stream");
-            tokio::spawn(async move {
-                QwtClient::handle_send_control_stream(tx, rx)
-                    .await
-                    .expect("server control")
-            });
+            self.handle_send_control_stream(tx, rx).await?;
         }
 
         // HEADERS frame will come to this stream
         if let Some(bidi) = self.conn.accept_bidirectional_stream().await? {
             debug!("Accepted bidi stream");
-            tokio::spawn(async move {
-                QwtClient::handle_bidirectional_request_stream(bidi)
-                    .await
-                    .expect("bidi request")
-            });
+            self.handle_bidirectional_request_stream(bidi).await?;
         }
 
         // This loop will concurrently accept new bidi and recv streams. I am not sure if
@@ -172,9 +163,7 @@ impl QwtClient {
         loop {
             tokio::select! {
                 Ok(Some(bidi)) = self.conn.accept_bidirectional_stream() => {
-                    tokio::spawn(async move {
-                        QwtClient::handle_bidirectional_stream(bidi).await.expect("bidi stream");
-                    });
+                    self.handle_bidirectional_stream(bidi).await?;
                 }
 
                 _ = sleep(Duration::from_millis(50)) => {}
@@ -182,9 +171,7 @@ impl QwtClient {
 
             tokio::select! {
                 Ok(Some(rx)) = self.conn.accept_receive_stream() => {
-                    tokio::spawn(async move {
-                        QwtClient::handle_receive_stream(rx).await.expect("unidi stream");
-                    });
+                    self.handle_receive_stream(rx).await?;
                 }
 
                 _ = sleep(Duration::from_millis(50)) => {}
@@ -193,199 +180,209 @@ impl QwtClient {
     }
 
     async fn handle_send_control_stream(
+        &mut self,
         mut tx: SendStream,
         mut rx: UnboundedReceiver<Bytes>,
     ) -> Result<(), Box<dyn Error>> {
         let typ = Bytes::from_static(&[0x00]);
         tx.send(typ).await?;
+
         debug!("Server opened control stream");
 
         // Listen on the mpsc and just blindly send the frames. We should probably put
         // this in a select and do something useful when it is just sitting here idle
-        while let Some(out) = rx.recv().await {
-            debug!("Got outgoing {} bytes", out.len());
-            tx.send(out).await?;
-        }
+        tokio::spawn(async move {
+            while let Some(out) = rx.recv().await {
+                debug!("Got outgoing {} bytes", out.len());
+                tx.send(out).await.expect("tx send");
+            }
+        });
 
         Ok(())
     }
 
     async fn handle_bidirectional_request_stream(
+        &mut self,
         mut bidi: BidirectionalStream,
     ) -> Result<(), Box<dyn Error>> {
-        let mut buf = [0; 1350];
-        let mut out = [0; 1350];
+        tokio::spawn(async move {
+            let mut buf = [0; 1350];
+            let mut out = [0; 1350];
 
-        while let Ok(Some(data)) = bidi.receive().await {
-            let mut data = Octets::with_slice(&data[..]);
+            while let Ok(Some(data)) = bidi.receive().await {
+                let mut data = Octets::with_slice(&data[..]);
 
-            // Parse frame type and length of payload
-            let typ = data.get_varint().unwrap();
-            let len = data.get_varint().unwrap();
-            let frm = data.get_bytes(len as usize)?;
+                // Parse frame type and length of payload
+                let typ = data.get_varint().unwrap();
+                let len = data.get_varint().unwrap();
+                let frm = data.get_bytes(len as usize).unwrap();
 
-            match typ {
-                0x01 => {
-                    // HEADERS Frame {
-                    //   Type (i) = 0x01,
-                    //   Length (i),
-                    //   Encoded Field Section (..),
-                    // }
+                match typ {
+                    0x01 => {
+                        // HEADERS Frame {
+                        //   Type (i) = 0x01,
+                        //   Length (i),
+                        //   Encoded Field Section (..),
+                        // }
 
-                    // I am lazy
-                    // TODO: replace with minimal QPACK implementation later
-                    let mut dec = qpack::Decoder::new();
-                    let hdrs = dec.decode(frm.buf(), 1024)?;
+                        // I am lazy
+                        // TODO: replace with minimal QPACK implementation later
+                        let mut dec = qpack::Decoder::new();
+                        let hdrs = dec.decode(frm.buf(), 1024).unwrap();
 
-                    // TODO: validate headers
-                    for hdr in hdrs {
-                        debug!("Got header {:?}", hdr);
+                        // TODO: validate headers
+                        for hdr in hdrs {
+                            debug!("Got header {:?}", hdr);
+                        }
+
+                        // TODO: handle non-200 cases
+                        let hdrs = vec![h3::Header::new(b":status", b"200")];
+
+                        debug!("Will send headers {:?}", hdrs);
+
+                        let mut enc = qpack::Encoder::new();
+                        let len = enc.encode(&hdrs, &mut buf[..]).unwrap();
+
+                        let len = {
+                            let mut out = OctetsMut::with_slice(&mut out[..]);
+
+                            out.put_varint(0x01).unwrap();
+                            out.put_varint(len as u64).unwrap();
+                            out.put_bytes(&buf[..len]).unwrap();
+
+                            out.off()
+                        };
+
+                        let out = Bytes::copy_from_slice(&out[..len]);
+
+                        bidi.send(out).await.unwrap();
+
+                        debug!("Sent {} bytes", len);
                     }
 
-                    // TODO: handle non-200 cases
-                    let hdrs = vec![h3::Header::new(b":status", b"200")];
-
-                    debug!("Will send headers {:?}", hdrs);
-
-                    let mut enc = qpack::Encoder::new();
-                    let len = enc.encode(&hdrs, &mut buf[..])?;
-
-                    let len = {
-                        let mut out = OctetsMut::with_slice(&mut out[..]);
-
-                        out.put_varint(0x01)?;
-                        out.put_varint(len as u64)?;
-                        out.put_bytes(&buf[..len])?;
-
-                        out.off()
-                    };
-
-                    let out = Bytes::copy_from_slice(&out[..len]);
-
-                    bidi.send(out).await?;
-
-                    debug!("Sent {} bytes", len);
-                }
-
-                unk => {
-                    panic!("Unknown frame type {}", unk);
+                    unk => {
+                        panic!("Unknown frame type {}", unk);
+                    }
                 }
             }
-        }
+        });
 
         Ok(())
     }
 
     async fn handle_receive_control_stream(
+        &mut self,
         mut rx: ReceiveStream,
         tx: UnboundedSender<Bytes>,
     ) -> Result<(), Box<dyn Error>> {
         // Ensure that it is indeed a control stream
         // NOTE: I think this might not be an invariant we can count on after the
         // WebTransport RFC is standardized
-        let stream_type = rx.read_u8().await?;
+        let stream_type = rx.read_u8().await.unwrap();
         assert_eq!(stream_type, 0x00);
 
         debug!("Client opened control stream");
 
-        let mut buf = [0; 1350];
-        let mut out = [0; 1350];
+        tokio::spawn(async move {
+            let mut buf = [0; 1350];
+            let mut out = [0; 1350];
 
-        let mut settings = HashMap::new();
-        let mut ver = None;
+            let mut settings = HashMap::new();
+            let mut ver = None;
 
-        while let Ok(Some(data)) = rx.receive().await {
-            let mut data = Octets::with_slice(&data[..]);
+            while let Ok(Some(data)) = rx.receive().await {
+                let mut data = Octets::with_slice(&data[..]);
 
-            // Parse frame type and length of payload
-            let typ = data.get_varint().unwrap();
-            let len = data.get_varint().unwrap();
-            let mut frm = data.get_bytes(len as usize)?;
+                // Parse frame type and length of payload
+                let typ = data.get_varint().unwrap();
+                let len = data.get_varint().unwrap();
+                let mut frm = data.get_bytes(len as usize).unwrap();
 
-            match typ {
-                0x04 => {
-                    // SETTINGS Frame {
-                    //  Type (i) = 0x04,
-                    //  Length (i),
-                    //  Setting (..) ...,
-                    // }
+                match typ {
+                    0x04 => {
+                        // SETTINGS Frame {
+                        //  Type (i) = 0x04,
+                        //  Length (i),
+                        //  Setting (..) ...,
+                        // }
 
-                    // Parse settings frame
-                    let mut prev = None;
-                    while let Ok(next) = frm.get_varint() {
-                        if let Some(key) = prev {
-                            // We have value for corresponding key
-                            settings.insert(key, next);
+                        // Parse settings frame
+                        let mut prev = None;
+                        while let Ok(next) = frm.get_varint() {
+                            if let Some(key) = prev {
+                                // We have value for corresponding key
+                                settings.insert(key, next);
 
-                            // Do some extra logic while we have the key and value
-                            if key == SETTINGS_ENABLE_WEBTRANSPORT {
-                                ver = Some("draft-02");
-                            } else if key == SETTINGS_WEBTRANSPORT_MAX_SESSIONS {
-                                ver = Some("draft-09");
+                                // Do some extra logic while we have the key and value
+                                if key == SETTINGS_ENABLE_WEBTRANSPORT {
+                                    ver = Some("draft-02");
+                                } else if key == SETTINGS_WEBTRANSPORT_MAX_SESSIONS {
+                                    ver = Some("draft-09");
+                                }
+
+                                // Reset so next iteration saves key
+                                prev = None;
+                            } else {
+                                prev = Some(next);
+                            }
+                        }
+
+                        if ver.is_none() {
+                            panic!("No WebTransport version specified");
+                        }
+
+                        let ver = ver.unwrap();
+
+                        // Create the actual setttings values for the response frame
+                        // 10 bytes should be fine for now
+                        let len = {
+                            let mut frm = OctetsMut::with_slice(&mut buf);
+
+                            // This is for draft-02 only
+                            if "draft-02" == ver {
+                                frm.put_varint(SETTINGS_ENABLE_WEBTRANSPORT).unwrap();
+                                frm.put_varint(1).unwrap();
+                            } else {
+                                frm.put_varint(SETTINGS_WEBTRANSPORT_MAX_SESSIONS).unwrap();
+                                frm.put_varint(1024).unwrap();
                             }
 
-                            // Reset so next iteration saves key
-                            prev = None;
-                        } else {
-                            prev = Some(next);
-                        }
+                            // Required in all WebTransport drafts
+                            frm.put_varint(SETTINGS_H3_DATAGRAM).unwrap();
+                            frm.put_varint(1).unwrap();
+
+                            frm.off()
+                        };
+
+                        // Add the type (0x04), len, and bytes computed above to send
+                        let len = {
+                            let mut out = OctetsMut::with_slice(&mut out);
+
+                            out.put_u8(0x04).unwrap();
+                            out.put_varint(len as u64).unwrap();
+
+                            out.put_bytes(&buf[..len]).unwrap();
+
+                            out.off()
+                        };
+
+                        // Bytes type makes it super convenient to send over a tokio mpsc
+                        let out = Bytes::copy_from_slice(&out[..len]);
+
+                        tx.send(out).unwrap();
                     }
 
-                    if ver.is_none() {
-                        panic!("No WebTransport version specified");
+                    unk => {
+                        panic!("Unknown frame type {}", unk);
                     }
-
-                    let ver = ver.unwrap();
-
-                    // Create the actual setttings values for the response frame
-                    // 10 bytes should be fine for now
-                    let len = {
-                        let mut frm = OctetsMut::with_slice(&mut buf);
-
-                        // This is for draft-02 only
-                        if "draft-02" == ver {
-                            frm.put_varint(SETTINGS_ENABLE_WEBTRANSPORT)?;
-                            frm.put_varint(1)?;
-                        } else {
-                            frm.put_varint(SETTINGS_WEBTRANSPORT_MAX_SESSIONS)?;
-                            frm.put_varint(1024)?;
-                        }
-
-                        // Required in all WebTransport drafts
-                        frm.put_varint(SETTINGS_H3_DATAGRAM)?;
-                        frm.put_varint(1)?;
-
-                        frm.off()
-                    };
-
-                    // Add the type (0x04), len, and bytes computed above to send
-                    let len = {
-                        let mut out = OctetsMut::with_slice(&mut out);
-
-                        out.put_u8(0x04)?;
-                        out.put_varint(len as u64)?;
-
-                        out.put_bytes(&buf[..len])?;
-
-                        out.off()
-                    };
-
-                    // Bytes type makes it super convenient to send over a tokio mpsc
-                    let out = Bytes::copy_from_slice(&out[..len]);
-
-                    tx.send(out)?;
-                }
-
-                unk => {
-                    panic!("Unknown frame type {}", unk);
                 }
             }
-        }
+        });
 
         Ok(())
     }
 
-    async fn handle_receive_stream(mut rx: ReceiveStream) -> Result<(), Box<dyn Error>> {
+    async fn handle_receive_stream(&mut self, mut rx: ReceiveStream) -> Result<(), Box<dyn Error>> {
         // HACK: read the varint frame type as two bytes
         let var = rx.read_u8().await?;
         assert_eq!(var, 0x40);
@@ -404,6 +401,7 @@ impl QwtClient {
     }
 
     async fn handle_bidirectional_stream(
+        &mut self,
         mut bidi: BidirectionalStream,
     ) -> Result<(), Box<dyn Error>> {
         // HACK: read the varint frame type as two bytes
